762f37bfdc82c1abf39db64be703cc268f14fc92
KTx735 <mygame1980@hotmail.com>
2022-08-03 13:22:33 -0600

    Update 8/3/2022
    - Lasers for Ship added
    - TimeLine added & fixed
    - Now the player is able to move (Translation & rotation)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,26 +8,24 @@ public class PlayerControls : MonoBehaviour
 
     [SerializeField] InputAction movement;
     [SerializeField] float controlSpeed = 20f;
-    [SerializeField] float xRange = 5f;
-    [SerializeField] float yRange = 5f;
+
+    /*[SerializeField]*/ float xRange = 8.6f;
+    /*[SerializeField]*/ float yRangePos = 6.6f;
+    /*[SerializeField]*/ float yRangeNeg = 3.1f;
+
+    [SerializeField] float positionPitchFactor = -3f;
+    [SerializeField] float controlPitchFactor = -3f;
+    [SerializeField] float positionYawFactor = -3f;
+    [SerializeField] float controlRollFactor = -3;
+
+    float xThrow;
+    float yThrow;
 
     // Update is called once per frame
     void Update()
     {
-        float xThrow = movement.ReadValue<Vector2>().x; //Movement in x
-        float yThrow = movement.ReadValue<Vector2>().y; //Movement in y
-
-        float xOffSet = xThrow * Time.deltaTime * controlSpeed; //Movement * FR (To keep movement independent) * Speed
-        float yOffset = yThrow * Time.deltaTime * controlSpeed; //Movement * FR (To keep movement independent) * Speed
-        
-        float rawXPos = transform.localPosition.x + xOffSet; //New position for x
-        float rawYPos = transform.localPosition.y + yOffset; //New position for y
-
-        //Clamp
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange); //Restrict Movement for x
-        //float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange); //Restrict Movement for y
-
-        transform.localPosition = new Vector3(clampedXPos, rawYPos, transform.localPosition.z);
+        ProcessTranslation();
+        ProcessRotation();
     }
 
     void OnEnable()
@@ -37,6 +35,37 @@ public class PlayerControls : MonoBehaviour
 
     void OnDisable()
     {
-         
+        movement.Disable();
     }
+
+    void ProcessTranslation() //Movement of the ship
+    {
+        xThrow = movement.ReadValue<Vector2>().x; //Movement in x
+        yThrow = movement.ReadValue<Vector2>().y; //Movement in y
+
+        float xOffSet = xThrow * Time.deltaTime * controlSpeed; //Movement * FR (To keep movement independent) * Speed
+        float yOffset = yThrow * Time.deltaTime * controlSpeed; //Movement * FR (To keep movement independent) * Speed
+        
+        float rawXPos = transform.localPosition.x + xOffSet; //New position for x
+        float rawYPos = transform.localPosition.y + yOffset; //New position for y
+
+        //Clamp
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange); //Restrict Movement for x
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRangeNeg, yRangePos); //Restrict Movement for y
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
 }
